f0825c0d38a36a5483cec87071372d8e3e2dad57
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-05-11 19:59:05 +0300

    Added logic for deploy.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+const { PRIVATE_KEY } = process.env;
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/iihAcJ-aZv3SHVMd2xKpKVn06Cf8CYLW",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
