45c5f0885b84d43425d6f5d32b9f6acbd9e00b2d
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-05-11 17:32:02 +0300

    wrote a simple smart contract in HelloWorld.sol

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+contract HelloWorld {
+    // Events
+    event MessageChanged(string oldmsg, string newmsg);
+
+    // State variable
+    string public message;
+
+    // Constructor
+    constructor(string memory firstmessage) {
+        message = firstmessage;
+    }
+
+    // Function to update message
+    function update(string memory newmessage) public {
+        string memory oldmsg = message;
+        message = newmessage;
+
+        emit MessageChanged(oldmsg, newmessage);
+    }
+}
+
+
+
